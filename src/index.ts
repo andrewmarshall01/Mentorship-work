254f7d12fe8865607f0eab344b517a1188dd7e29
Andrew Marshall <andrew.marshall@frasers.group>
2026-02-06 14:45:23 +0000

    promise and res added for express js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-const userName: string = "Andrew"
+import express from 'express'
+import { resolve } from 'node:dns'
+const app = express()
+const port = 3000
 
-console.log("Hello, ", userName, "!")
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+new Promise(function(resolve, _reject) {
+    app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+    resolve(app)
+    })
+})
