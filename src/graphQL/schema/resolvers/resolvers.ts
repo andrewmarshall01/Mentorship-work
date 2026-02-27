fad7e4dee802596f29721d0a18e8fea8b631afaf
andrewmarshall01 <andrew.marshall@frasers.group>
2026-02-27 17:31:09 +0000

    added more fields

--- src/graphQL/schema/resolvers/resolvers.ts
+++ src/graphQL/schema/resolvers/resolvers.ts
@@ -1,3 +1,10 @@
+enum DificultyRanking {
+  "BEGGINER",
+  "INTERMEDIATE",
+  "HARD",
+  "EXPERT",
+}
+
 export const resolvers = {
   Query: {
     helloWorld: () => "hello world",
@@ -5,6 +12,19 @@ export const resolvers = {
       name: args.name,
       age: 20,
       job: "job",
+    }),
+    getCarByOwnerName: (_: unknown, args: { ownerName: string }) => ({
+      ownerName: args.ownerName,
+      licencePlate: "FD09 XPX",
+      colour: "white",
+    }),
+    getHikingTrailByDifficulty: (
+      _: unknown,
+      args: { difficulty: DificultyRanking },
+    ) => ({
+      distance: 10.2,
+      elevation: 1.1,
+      difficulty: args.difficulty,
     }),
   },
 };
