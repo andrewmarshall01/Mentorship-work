17b33a6310fec73a2a6d2590ba7c0d98e1e7b1a6
andrewmarshall01 <andrew.marshall@frasers.group>
2026-02-27 14:56:05 +0000

    added person type and resolver

--- src/graphQL/schema/resolvers/resolvers.ts
+++ src/graphQL/schema/resolvers/resolvers.ts
@@ -1,3 +1,10 @@
 export const resolvers = {
-  Query: { helloWorld: () => "hello world" },
+  Query: {
+    helloWorld: () => "hello world",
+    getPersonByName: (_: unknown, args: { name: string }) => ({
+      name: args.name,
+      age: 20,
+      job: "job",
+    }),
+  },
 };
