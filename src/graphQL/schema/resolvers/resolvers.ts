72926ee5eea87e5c3855bcdd901d964c179cc70c
andrewmarshall01 <andrew.marshall@frasers.group>
2026-03-02 11:44:09 +0000

    added new fields and resolvers

--- src/graphQL/schema/resolvers/resolvers.ts
+++ src/graphQL/schema/resolvers/resolvers.ts
@@ -1,9 +1,17 @@
+import { GraphQLError } from "graphql";
+
 enum DificultyRanking {
   "BEGGINER",
   "INTERMEDIATE",
   "HARD",
   "EXPERT",
 }
+
+type CarArgs = {
+  name: string;
+  age: number;
+  job: string;
+};
 
 export const resolvers = {
   Query: {
@@ -13,8 +21,12 @@ export const resolvers = {
       age: 20,
       job: "job",
     }),
-    getCarByOwnerName: (_: unknown, args: { ownerName: string }) => ({
-      ownerName: args.ownerName,
+    getCarByOwner: (_: unknown, args: CarArgs) => ({
+      owner: {
+        name: args.name,
+        age: args.age,
+        job: args.job,
+      },
       licencePlate: "FD09 XPX",
       colour: "white",
     }),
@@ -25,6 +37,25 @@ export const resolvers = {
       distance: 10.2,
       elevation: 1.1,
       difficulty: args.difficulty,
+      parking: true,
+      rating: 5,
     }),
+    getHikingTrailsByRating: (_: unknown, args: { rating: number }) => {
+      if (
+        args.rating > 5 ||
+        args.rating < 1 ||
+        !Number.isInteger(args.rating)
+      ) {
+        throw new GraphQLError("rating must be an integer between 1 and five");
+      }
+      return {
+        distance: 10.2,
+        elevation: 1.1,
+        difficulty: "HARD",
+        parking: true,
+        rating: args.rating,
+      };
+    },
   },
 };
+// Question for Charlotte: when an arg will be a described type like in car what is best practice
